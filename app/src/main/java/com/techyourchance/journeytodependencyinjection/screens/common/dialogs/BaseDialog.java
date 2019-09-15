deadad25cec8187504794d2bc8babae65ac3ab50
StephenGemin <45926479+StephenGemin@users.noreply.github.com>
2019-09-14 21:15:08 -0400

    Create base classes
    Creating:
    * BaseFragment
    * BaseDialog
    
    Exposing PresentationComponent to these new base classes

---
+++ app/src/main/java/com/techyourchance/journeytodependencyinjection/screens/common/dialogs/BaseDialog.java
@@ -0,0 +1,36 @@
+package com.techyourchance.journeytodependencyinjection.screens.common.dialogs;
+
+import android.support.annotation.UiThread;
+import android.support.v4.app.DialogFragment;
+
+import com.techyourchance.journeytodependencyinjection.common.activities.BaseApplication;
+import com.techyourchance.journeytodependencyinjection.common.di.component.ApplicationComponent;
+import com.techyourchance.journeytodependencyinjection.common.di.component.PresentationComponent;
+import com.techyourchance.journeytodependencyinjection.common.di.module.PresentationModule;
+
+/**
+ * Created by Stephen Gemin on 9/14/2019
+ */
+public class BaseDialog extends DialogFragment {
+    private boolean mIsInjectorUsed;
+
+    /**
+     * Has activity lifecycle
+     */
+    @UiThread
+    protected PresentationComponent getPresentationComponent() {
+        if (mIsInjectorUsed) {
+            throw new RuntimeException("There is no need to use injector more than once");
+        }
+        mIsInjectorUsed = true;
+        return getApplicationComponent()
+                .newPresentationComponent(new PresentationModule(getActivity()));
+    }
+
+    /**
+     * Has application lifecycle
+     */
+    private ApplicationComponent getApplicationComponent() {
+        return ((BaseApplication) getActivity().getApplication()).getApplicationComponent();
+    }
+}
